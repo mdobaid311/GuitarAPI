79d4bfac8281e073839d4b98777e20f8e9f194c8
mdobaid311 <mdobaid311@gmail.com>
2023-04-14 15:00:26 +0530

    by year route added

--- controllers/active-line-status-details.js
+++ controllers/active-line-status-details.js
@@ -8,7 +8,7 @@ const getOriginalOrderTotalAmount = async (req, res) => {
   try {
     if (year) {
       const result = await client.execute(
-        `select sum(invoiced_line_total) as original_orders_total from temp_table where order_date >= '${year}-01-01 00:00:00' and order_date <= '${year}-12-31 23:59:59' ALLOW FILTERING`
+        `select sum(original_order_total_amount) as original_orders_total from temp_table where order_date >= '${year}-01-01 00:00:00' and order_date <= '${year}-12-31 23:59:59' ALLOW FILTERING`
       );
       res.status(200).json(result.rows);
       return;
@@ -17,28 +17,28 @@ const getOriginalOrderTotalAmount = async (req, res) => {
     if (!start_date && !end_date) {
       console.log("first");
       const result = await client.execute(
-        "select sum(invoiced_line_total) as original_orders_total from temp_table ALLOW FILTERING"
+        "select sum(original_order_total_amount) as original_orders_total from temp_table ALLOW FILTERING"
       );
 
       res.status(200).json(result.rows);
     } else if (start_date && end_date) {
       console.log("second");
       const result = await client.execute(
-        `select sum(invoiced_line_total) as original_orders_total from temp_table where order_date >= ${start_date} and order_date <= ${end_date} ALLOW FILTERING`
+        `select sum(original_order_total_amount) as original_orders_total from temp_table where order_date >= ${start_date} and order_date <= ${end_date} ALLOW FILTERING`
       );
 
       res.status(200).json(result.rows);
     } else if (start_date) {
       console.log("third");
       const result = await client.execute(
-        `select sum(invoiced_line_total) as original_orders_total from temp_table where order_date >= ${start_date} ALLOW FILTERING`
+        `select sum(original_order_total_amount) as original_orders_total from temp_table where order_date >= ${start_date} ALLOW FILTERING`
       );
 
       res.status(200).json(result.rows);
     } else if (end_date) {
       console.log("last");
       const result = await client.execute(
-        `select sum(invoiced_line_total) as original_orders_total from temp_table where order_date <= ${end_date} ALLOW FILTERING`
+        `select sum(original_order_total_amount) as original_orders_total from temp_table where order_date <= ${end_date} ALLOW FILTERING`
       );
 
       res.status(200).json(result.rows);
@@ -60,11 +60,51 @@ const getOriginalOrderTotalByMonth = async (req, res) => {
         .add(i, "months")
         .format("YYYY-MM-DD HH:mm:ss");
 
+      const end_date = moment(startDate)
+        .add(1, "months")
+        .format("YYYY-MM-DD HH:mm:ss");
+
       const result = await client.execute(
-        `select sum(invoiced_line_total) as original_orders_total from temp_table where order_date > '${startDate}' ALLOW FILTERING`
+        `select sum(original_order_total_amount) as original_orders_total from temp_table where order_date > '${startDate}' and order_date < '${end_date}' ALLOW FILTERING`
       );
+
       results.push({
         monthName: moment(startDate).format("MMMM"),
+        total: result.rows[0].original_orders_total,
+      });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(401).json(error);
+  }
+};
+
+const getOriginalOrderTotalByYear = async (req, res) => {
+  const results = [];
+
+  // const currentYear = moment().format("YYYY");
+  const currentYear = moment("2022").format("YYYY");
+
+  try {
+    for (let i = 0; i < 8; i++) {
+      const start_date = moment(currentYear)
+        .subtract(i, "year")
+        .format("YYYY-MM-DD HH:mm:ss");
+
+      const end_date = moment(start_date)
+        .subtract(1, "year")
+        .format("YYYY-MM-DD HH:mm:ss");
+
+      console.log(start_date, end_date);
+
+      const result = await client.execute(
+        `select sum(original_order_total_amount) as original_orders_total from temp_table where order_date < '${start_date}' and order_date > '${end_date}' ALLOW FILTERING`
+      );
+      console.log(
+        `select sum(original_order_total_amount) as original_orders_total from temp_table where order_date < '${start_date}' and order_date > '${end_date}' ALLOW FILTERING`
+      );
+      results.push({
+        year: moment(end_date).format("YYYY"),
         total: result.rows[0].original_orders_total,
       });
     }
@@ -77,4 +117,5 @@ const getOriginalOrderTotalByMonth = async (req, res) => {
 module.exports = {
   getOriginalOrderTotalAmount,
   getOriginalOrderTotalByMonth,
+  getOriginalOrderTotalByYear,
 };
--- routes/active-line-status-details.js
+++ routes/active-line-status-details.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const {
-  getOriginalOrderTotalAmount, getOriginalOrderTotalByMonth,
+  getOriginalOrderTotalAmount,
+  getOriginalOrderTotalByMonth,
+  getOriginalOrderTotalByYear,
 } = require("../controllers/active-line-status-details");
 
 const router = express.Router();
 
 router.get("/original-order-total", getOriginalOrderTotalAmount);
 router.get("/original-order-total-by-month", getOriginalOrderTotalByMonth);
+router.get("/original-order-total-by-year", getOriginalOrderTotalByYear);
 
 module.exports = router;
