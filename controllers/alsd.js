6384d36f413fc0e00f65d8eac8b6660f67a21975
mdobaid311 <mdobaid311@gmail.com>
2023-04-27 22:26:57 +0530

    new commit

--- controllers/alsd.js
+++ controllers/alsd.js
@@ -196,10 +196,247 @@ const getOriginalOrderTotalByHour = async (req, res) => {
   }
 };
 
+const getOriginalOrderTotalByTenMinRange = async (req, res) => {
+  const { startDate, endDate } = req.query;
+  const startYear = moment(startDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const startMonth = moment(startDate, "YYYY-MM-DD HH:mm").format("MM");
+  const startDay = moment(startDate, "YYYY-MM-DD HH:mm").format("DD");
+  const startHour = moment(startDate, "YYYY-MM-DD HH:mm").format("HH");
+  const endYear = moment(endDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const endMonth = moment(endDate, "YYYY-MM-DD HH:mm").format("MM");
+  const endDay = moment(endDate, "YYYY-MM-DD HH:mm").format("DD");
+  const endHour = moment(endDate, "YYYY-MM-DD HH:mm").format("HH");
+
+  try {
+    const result = await client.execute(
+      `select * from alsd_aggregated where year>=${startYear} and month>=${startMonth} and day>=${startDay} and hour>=${startHour} and year<=${endYear} and month<=${endMonth} and day<=${endDay} and hour<=${endHour} ALLOW FILTERING`,
+      null,
+      {
+        prepare: true,
+        fetchSize: 10000,
+      }
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(200).json({ totalAmount: 0, data: [] });
+    }
+    const tenMinOrderTotal = result.rows.reduce((acc, order) => {
+      const ten_min = order.ten_min.low;
+      const total = order.original_order_total_amount.low;
+      acc[ten_min] = (acc[ten_min] || 0) + total;
+      return acc;
+    }, {});
+
+    const totalAmount = Object.values(tenMinOrderTotal).reduce(
+      (acc, total) => acc + total
+    );
+
+    const tenMinOrderTotalArray = Object.keys(tenMinOrderTotal).map(
+      (ten_min) => {
+        console.log(ten_min);
+        return {
+          ten_min: moment(ten_min, "mm").format("mm"),
+          total: tenMinOrderTotal[ten_min],
+        };
+      }
+    );
+    res.status(200).json({ totalAmount, data: tenMinOrderTotalArray });
+  } catch (error) {
+    res.status(401).json(error);
+  }
+};
+
+const getOriginalOrderTotalByHourRange = async (req, res) => {
+  const { startDate, endDate } = req.query;
+  const startYear = moment(startDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const startMonth = moment(startDate, "YYYY-MM-DD HH:mm").format("MM");
+  const startDay = moment(startDate, "YYYY-MM-DD HH:mm").format("DD");
+  const startHour = moment(startDate, "YYYY-MM-DD HH:mm").format("HH");
+  const endYear = moment(endDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const endMonth = moment(endDate, "YYYY-MM-DD HH:mm").format("MM");
+  const endDay = moment(endDate, "YYYY-MM-DD HH:mm").format("DD");
+  const endHour = moment(endDate, "YYYY-MM-DD HH:mm").format("HH");
+
+  try {
+    const result = await client.execute(
+      `select * from alsd_aggregated where year>=${startYear} and month>=${startMonth} and day>=${startDay} and hour>=${startHour} and year<=${endYear} and month<=${endMonth} and day<=${endDay} and hour<=${endHour} ALLOW FILTERING`,
+      null,
+      {
+        prepare: true,
+        fetchSize: 10000,
+      }
+    );
+    if (result.rows.length === 0) {
+      return res.status(200).json({ totalAmount: 0, data: [] });
+    }
+
+    const HourlyOrderTotal = result.rows.reduce((acc, order) => {
+      const hour = order.hour.low;
+      const total = order.original_order_total_amount.low;
+      acc[hour] = (acc[hour] || 0) + total;
+      return acc;
+    }, {});
+
+    const totalAmount = Object.values(HourlyOrderTotal).reduce(
+      (acc, total) => acc + total
+    );
+
+    const HourlyOrderTotalArray = Object.keys(HourlyOrderTotal).map((hour) => {
+      console.log(hour);
+      return {
+        hour: moment(hour, "HH").format("HH A"),
+        total: HourlyOrderTotal[hour],
+      };
+    });
+    res.status(200).json({ totalAmount, data: HourlyOrderTotalArray });
+  } catch (error) {
+    res.status(401).json(error);
+  }
+};
+
+const getOriginalOrderTotalByDayRange = async (req, res) => {
+  const { startDate, endDate } = req.query;
+  const startYear = moment(startDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const startMonth = moment(startDate, "YYYY-MM-DD HH:mm").format("MM");
+  const startDay = moment(startDate, "YYYY-MM-DD HH:mm").format("DD");
+  const endYear = moment(endDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const endMonth = moment(endDate, "YYYY-MM-DD HH:mm").format("MM");
+  const endDay = moment(endDate, "YYYY-MM-DD HH:mm").format("DD");
+
+  try {
+    const result = await client.execute(
+      `select * from alsd_aggregated where year>=${startYear} and month>=${startMonth} and day>=${startDay} and year<=${endYear} and month<=${endMonth} and day<=${endDay}  ALLOW FILTERING`,
+      null,
+      {
+        prepare: true,
+        fetchSize: 10000,
+      }
+    );
+    if (result.rows.length === 0) {
+      return res.status(200).json({ totalAmount: 0, data: [] });
+    }
+    const daylyOrderTotal = result.rows.reduce((acc, order) => {
+      const day = order.day.low;
+      const total = order.original_order_total_amount.low;
+      acc[day] = (acc[day] || 0) + total;
+      return acc;
+    }, {});
+
+    const totalAmount = Object.values(daylyOrderTotal).reduce(
+      (acc, total) => acc + total
+    );
+
+    const daylyOrderTotalArray = Object.keys(daylyOrderTotal).map((day) => {
+      console.log(day);
+      return {
+        day: moment(day, "DD").format("DD"),
+        total: daylyOrderTotal[day],
+      };
+    });
+    res.status(200).json({ totalAmount, data: daylyOrderTotalArray });
+  } catch (error) {
+    res.status(401).json(error);
+  }
+};
+
+const getOriginalOrderTotalByMonthRange = async (req, res) => {
+  const { startDate, endDate } = req.query;
+  const startYear = moment(startDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const startMonth = moment(startDate, "YYYY-MM-DD HH:mm").format("MM");
+  const endYear = moment(endDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const endMonth = moment(endDate, "YYYY-MM-DD HH:mm").format("MM");
+  console.log(startDate, endDate);
+  console.log(
+    `select * from alsd_aggregated where year>=${startYear}  and year<=${endYear}  ALLOW FILTERING`
+  );
+  try {
+    const result = await client.execute(
+      `select * from alsd_aggregated where year>=${startYear} and month>= ${startMonth} and year<=${endYear}  ALLOW FILTERING`,
+      null,
+      {
+        prepare: true,
+        fetchSize: 10000,
+      }
+    );
+    if (result.rows.length === 0) {
+      return res.status(200).json({ totalAmount: 0, data: [] });
+    }
+    const monthlyOrderTotal = result.rows.reduce((acc, order) => {
+      const month = order.month.low;
+      const total = order.original_order_total_amount.low;
+      acc[month] = (acc[month] || 0) + total;
+      return acc;
+    }, {});
+
+    const totalAmount = Object.values(monthlyOrderTotal).reduce(
+      (acc, total) => acc + total
+    );
+
+    const monthlyOrderTotalArray = Object.keys(monthlyOrderTotal).map(
+      (month) => {
+        console.log(month);
+        return {
+          month: moment(month, "MM").format("MM"),
+          total: monthlyOrderTotal[month],
+        };
+      }
+    );
+    res.status(200).json({ totalAmount, data: monthlyOrderTotalArray });
+  } catch (error) {
+    res.status(401).json(error);
+  }
+};
+
+const getOriginalOrderTotalByYearRange = async (req, res) => {
+  const { startDate, endDate } = req.query;
+  const startYear = moment(startDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const startMonth = moment(startDate, "YYYY-MM-DD HH:mm").format("MM");
+  const endYear = moment(endDate, "YYYY-MM-DD HH:mm").format("YYYY");
+  const endMonth = moment(endDate, "YYYY-MM-DD HH:mm").format("MM");
+  try {
+    const result = await client.execute(
+      `select * from alsd_aggregated where year>=${startYear} and month>=${startMonth}  and year<=${endYear} and month<=${endMonth}   ALLOW FILTERING`,
+      null,
+      {
+        prepare: true,
+        fetchSize: 10000,
+      }
+    );
+    if (result.rows.length === 0) {
+      return res.status(200).json({ totalAmount: 0, data: [] });
+    }
+    const yearlyOrderTotal = result.rows.reduce((acc, order) => {
+      const year = order.year.low;
+      const total = order.original_order_total_amount.low;
+      acc[year] = (acc[year] || 0) + total;
+      return acc;
+    }, {});
+
+    const totalAmount = Object.values(yearlyOrderTotal).reduce(
+      (acc, total) => acc + total
+    );
+
+    const yearlyOrderTotalArray = Object.keys(yearlyOrderTotal).map((year) => {
+      return {
+        year: moment(year, "YYYY").format("YYYY"),
+        total: yearlyOrderTotal[year],
+      };
+    });
+    res.status(200).json({ totalAmount, data: yearlyOrderTotalArray });
+  } catch (error) {
+    res.status(401).json(error);
+  }
+};
+
 module.exports = {
   getOriginalOrderTotalAmount,
   getOriginalOrderTotalByMonth,
   getOriginalOrderTotalByYear,
   getOriginalOrderTotalByDay,
   getOriginalOrderTotalByHour,
+  getOriginalOrderTotalByTenMinRange,
+  getOriginalOrderTotalByHourRange,
+  getOriginalOrderTotalByDayRange,
+  getOriginalOrderTotalByMonthRange,
+  getOriginalOrderTotalByYearRange,
 };
--- routes/active-line-status-details.js
+++ routes/active-line-status-details.js
@@ -7,12 +7,17 @@ const express = require("express");
 //   getOriginalOrderTotalByHour,
 // } = require("../controllers/active-line-status-details");
 
- const {
+const {
   getOriginalOrderTotalAmount,
   getOriginalOrderTotalByMonth,
   getOriginalOrderTotalByYear,
   getOriginalOrderTotalByDay,
   getOriginalOrderTotalByHour,
+  getOriginalOrderTotalByTenMinRange,
+  getOriginalOrderTotalByHourRange,
+  getOriginalOrderTotalByDayRange,
+  getOriginalOrderTotalByMonthRange,
+  getOriginalOrderTotalByYearRange,
 } = require("../controllers/alsd");
 
 const router = express.Router();
@@ -22,5 +27,25 @@ router.get("/original-order-total-by-month", getOriginalOrderTotalByMonth);
 router.get("/original-order-total-by-year", getOriginalOrderTotalByYear);
 router.get("/original-order-total-by-day", getOriginalOrderTotalByDay);
 router.get("/original-order-total-by-hour", getOriginalOrderTotalByHour);
+router.get(
+  "/original-order-total-by-tenmin-range",
+  getOriginalOrderTotalByTenMinRange
+);
+router.get(
+  "/original-order-total-by-hour-range",
+  getOriginalOrderTotalByHourRange
+);
+router.get(
+  "/original-order-total-by-day-range",
+  getOriginalOrderTotalByDayRange
+);
+router.get(
+  "/original-order-total-by-month-range",
+  getOriginalOrderTotalByMonthRange
+);
+router.get(
+  "/original-order-total-by-year-range",
+  getOriginalOrderTotalByYearRange
+);
 
 module.exports = router;
