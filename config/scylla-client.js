9daf7789d6ffceb054d57f86fd52b43e5929d7f1
mdobaid311 <mdobaid311@gmail.com>
2023-04-28 15:12:24 +0530

    new commit

--- config/scylla-client.js
+++ config/scylla-client.js
@@ -5,6 +5,10 @@ try {
     contactPoints: ["43.204.234.254"],
     localDataCenter: "ap-south",
     keyspace: "guitardb",
+    socketOptions: {
+      connectTimeout: 100000,
+      readTimeout: 100000,
+    },
   });
 } catch (error) {
   console.log(error);
