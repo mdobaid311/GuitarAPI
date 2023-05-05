6bcd7695da47019c1230e5993ce1ccc5d570e234
mdobaid311 <mdobaid311@gmail.com>
2023-05-05 22:40:08 +0530

    sql endpoints added

--- v2/controllers/active_line_status_details.js
+++ v2/controllers/active_line_status_details.js
@@ -77,17 +77,29 @@ const getSalesData = async (req, res) => {
 const getSalesDataByRange = async (req, res) => {
   const { start_date, end_date } = req.query;
 
-  const differeceInDays = moment(end_date).diff(moment(start_date), "days");
+  const differenceInDays = moment(end_date).diff(moment(start_date), "days");
 
   let query = `SELECT DATE(order_date_parsed) AS datetime, SUM(original_order_total_amount) AS original_order_total_amount, SUM(status_quantity) AS status_quantity FROM active_line_status where order_date_parsed BETWEEN '${start_date}' AND '${end_date}' GROUP BY datetime order by datetime;`;
 
-  console.log(differeceInDays, "differeceInDays")
-  if (differeceInDays > 31) {
-    const startMonthAndYear = moment(start_date).format("DD - MM - YY");
-    const endMonthAndYear = moment(end_date).format("DD - MM - YY");
-    console.log(startMonthAndYear, endMonthAndYear, "startMonthAndYear, endMonthAndYear")
-    query = `SELECT DATE_FORMAT(order_date_parsed, '%Y-%m') AS datetime, SUM(original_order_total_amount) AS original_order_total_amount, SUM (status_quantity) AS status_quantity FROM active_line_status where DATE_FORMAT(order_date_parsed, '%d - %m - %y') BETWEEN '${startMonthAndYear}' AND '${endMonthAndYear}' GROUP BY datetime order by datetime`;
-    // console.log(query, "query")
+  console.log(differenceInDays, "differenceInDays ");
+  // start_date = 2023-01-01
+  // end_date = 2023-02-15
+  if (differenceInDays > 31 && differenceInDays < 365) {
+    query = `SELECT DATE_FORMAT(order_date_parsed, '%Y-%m') AS datetime, 
+                    SUM(original_order_total_amount) AS original_order_total_amount,
+                    SUM(status_quantity) AS status_quantity
+             FROM active_line_status 
+             WHERE order_date_parsed >= '${start_date}' AND order_date_parsed <= '${end_date}' 
+             GROUP BY datetime 
+             ORDER BY datetime`;
+  } else if (differenceInDays > 365) {
+    query = `SELECT DATE_FORMAT(order_date_parsed, '%Y') AS datetime, 
+                    SUM(original_order_total_amount) AS original_order_total_amount,
+                    SUM(status_quantity) AS status_quantity
+             FROM active_line_status 
+             WHERE order_date_parsed >= '${start_date}' AND order_date_parsed <= '${end_date}' 
+             GROUP BY datetime 
+             ORDER BY datetime`;
   }
 
   connection.query(query, (err, result) => {
@@ -106,7 +118,12 @@ const getSalesDataByRange = async (req, res) => {
         total_quantity,
         series: result.map((item) => {
           return {
-            datetime: moment(item.datetime).format("DD-MMM-YYYY"),
+            datetime:
+              differenceInDays > 31 && differenceInDays < 365
+                ? moment(item.datetime).format("MMM-YYYY")
+                : differenceInDays > 365
+                ? moment(item.datetime).format("YYYY")
+                : moment(item.datetime).format("DD-MMM-YYYY"),
             original_order_total_amount: item.original_order_total_amount,
             status_quantity: item.status_quantity,
           };
