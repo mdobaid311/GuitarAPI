e897a5f38276945c4ad4e3ed2c255ec9b9f3a488
mdobaid311 <mdobaid311@gmail.com>
2023-05-23 18:44:52 +0530

    new commit

--- v2/controllers/tables.js
+++ v2/controllers/tables.js
@@ -33,223 +33,227 @@ const getFullSalesData = (req, res) => {
   `;
   console.log(query);
 
-  client.query(query, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send(err);
-      return;
-    } else {
-      console.log(new Date());
-      console.log(result[7].rows);
+  try {
+    client.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+        return;
+      } else {
+        console.log(new Date());
+        console.log(result[7].rows);
 
-      const groupedChartSeries = result[2].rows.reduce((acc, order) => {
-        const enterpriseKey = order.enterprise_key;
+        const groupedChartSeries = result[2].rows.reduce((acc, order) => {
+          const enterpriseKey = order.enterprise_key;
 
-        if (!acc[enterpriseKey]) {
-          acc[enterpriseKey] = {
-            enterprise_key: enterpriseKey,
-            series: [],
-          };
-        }
-        // "2023-01-21T18:30:00.000Z"
-        acc[enterpriseKey].series.push({
-          enterprise_key: order.enterprise_key,
-          datetime:
-            +intervaltime === 86400
-              ? moment(order.datetime).format("YYYY-MM-DD")
-              : +intervaltime === 3600
-              ? moment(order.datetime).format("DD MMM HH:mm")
-              : moment(order.datetime).format("YYYY-MM-DD HH:mm:ss"),
-
-          original_order_total_amount: +order.original_order_total_amount,
-          line_ordered_qty: +order.line_ordered_qty,
-        });
-
-        return acc;
-      }, {});
-
-      const groupedSalesCategoriesData = result[3].rows.reduce(
-        (result, item) => {
-          const { enterprise_key, order_capture_channel, item_info } = item;
-
-          // Find existing enterprise key group
-          let enterpriseKeyGroup = result.find(
-            (group) => group.name === enterprise_key
-          );
-
-          if (!enterpriseKeyGroup) {
-            // Create a new enterprise key group if not found
-            enterpriseKeyGroup = {
-              name: enterprise_key,
-              original_order_total_amount: 0,
-              line_ordered_qty: 0,
-              ORDER_CAPTURE_CHANNEL_GROUPED: [],
-              ITEM_INFO_GROUPED: [],
+          if (!acc[enterpriseKey]) {
+            acc[enterpriseKey] = {
+              enterprise_key: enterpriseKey,
+              series: [],
             };
-            result.push(enterpriseKeyGroup);
           }
+          // "2023-01-21T18:30:00.000Z"
+          acc[enterpriseKey].series.push({
+            enterprise_key: order.enterprise_key,
+            datetime:
+              +intervaltime === 86400
+                ? moment(order.datetime).format("YYYY-MM-DD")
+                : +intervaltime === 3600
+                ? moment(order.datetime).format("DD MMM HH:mm")
+                : moment(order.datetime).format("YYYY-MM-DD HH:mm:ss"),
 
-          // Update original_order_total_amount and line_ordered_qty
-          enterpriseKeyGroup.original_order_total_amount += parseInt(
-            item.original_order_total_amount
-          );
-          enterpriseKeyGroup.line_ordered_qty += parseInt(
-            item.line_ordered_qty
-          );
+            original_order_total_amount: +order.original_order_total_amount,
+            line_ordered_qty: +order.line_ordered_qty,
+          });
 
-          // Group by order_capture_channel
-          let orderCaptureChannelGroup =
-            enterpriseKeyGroup.ORDER_CAPTURE_CHANNEL_GROUPED.find(
-              (group) => group.name === order_capture_channel
+          return acc;
+        }, {});
+
+        const groupedSalesCategoriesData = result[3].rows.reduce(
+          (result, item) => {
+            const { enterprise_key, order_capture_channel, item_info } = item;
+
+            // Find existing enterprise key group
+            let enterpriseKeyGroup = result.find(
+              (group) => group.name === enterprise_key
             );
 
-          if (!orderCaptureChannelGroup) {
-            orderCaptureChannelGroup = {
-              name: order_capture_channel,
-              original_order_total_amount: 0,
-              line_ordered_qty: 0,
-            };
-            enterpriseKeyGroup.ORDER_CAPTURE_CHANNEL_GROUPED.push(
-              orderCaptureChannelGroup
+            if (!enterpriseKeyGroup) {
+              // Create a new enterprise key group if not found
+              enterpriseKeyGroup = {
+                name: enterprise_key,
+                original_order_total_amount: 0,
+                line_ordered_qty: 0,
+                ORDER_CAPTURE_CHANNEL_GROUPED: [],
+                ITEM_INFO_GROUPED: [],
+              };
+              result.push(enterpriseKeyGroup);
+            }
+
+            // Update original_order_total_amount and line_ordered_qty
+            enterpriseKeyGroup.original_order_total_amount += parseInt(
+              item.original_order_total_amount
             );
+            enterpriseKeyGroup.line_ordered_qty += parseInt(
+              item.line_ordered_qty
+            );
+
+            // Group by order_capture_channel
+            let orderCaptureChannelGroup =
+              enterpriseKeyGroup.ORDER_CAPTURE_CHANNEL_GROUPED.find(
+                (group) => group.name === order_capture_channel
+              );
+
+            if (!orderCaptureChannelGroup) {
+              orderCaptureChannelGroup = {
+                name: order_capture_channel,
+                original_order_total_amount: 0,
+                line_ordered_qty: 0,
+              };
+              enterpriseKeyGroup.ORDER_CAPTURE_CHANNEL_GROUPED.push(
+                orderCaptureChannelGroup
+              );
+            }
+
+            // Update original_order_total_amount and line_ordered_qty within the order_capture_channel group
+            orderCaptureChannelGroup.original_order_total_amount += parseInt(
+              item.original_order_total_amount
+            );
+            orderCaptureChannelGroup.line_ordered_qty += parseInt(
+              item.line_ordered_qty
+            );
+
+            // Group by ITEM_INFO
+            let itemInfoGroup = enterpriseKeyGroup.ITEM_INFO_GROUPED.find(
+              (group) => group.name === item_info
+            );
+
+            if (!itemInfoGroup) {
+              itemInfoGroup = {
+                name: item_info,
+                original_order_total_amount: 0,
+                line_ordered_qty: 0,
+              };
+              enterpriseKeyGroup.ITEM_INFO_GROUPED.push(itemInfoGroup);
+            }
+
+            // Update original_order_total_amount and line_ordered_qty within the ITEM_INFO group
+            itemInfoGroup.original_order_total_amount += parseInt(
+              item.original_order_total_amount
+            );
+            itemInfoGroup.line_ordered_qty += parseInt(item.line_ordered_qty);
+
+            return result;
+          },
+          []
+        );
+
+        const groupedTopItemsData = result[4].rows.reduce((result, item) => {
+          const { enterprise_key, ...rest } = item;
+
+          if (!result[enterprise_key]) {
+            result[enterprise_key] = [];
           }
 
-          // Update original_order_total_amount and line_ordered_qty within the order_capture_channel group
-          orderCaptureChannelGroup.original_order_total_amount += parseInt(
-            item.original_order_total_amount
-          );
-          orderCaptureChannelGroup.line_ordered_qty += parseInt(
-            item.line_ordered_qty
-          );
-
-          // Group by ITEM_INFO
-          let itemInfoGroup = enterpriseKeyGroup.ITEM_INFO_GROUPED.find(
-            (group) => group.name === item_info
-          );
-
-          if (!itemInfoGroup) {
-            itemInfoGroup = {
-              name: item_info,
-              original_order_total_amount: 0,
-              line_ordered_qty: 0,
-            };
-            enterpriseKeyGroup.ITEM_INFO_GROUPED.push(itemInfoGroup);
-          }
-
-          // Update original_order_total_amount and line_ordered_qty within the ITEM_INFO group
-          itemInfoGroup.original_order_total_amount += parseInt(
-            item.original_order_total_amount
-          );
-          itemInfoGroup.line_ordered_qty += parseInt(item.line_ordered_qty);
-
+          result[enterprise_key].push(rest);
           return result;
-        },
-        []
-      );
+        }, {});
 
-      const groupedTopItemsData = result[4].rows.reduce((result, item) => {
-        const { enterprise_key, ...rest } = item;
+        const shippingCost = {
+          gc: result[5].rows.filter(
+            (item) => item.enterprise_key === "GC" && item.is_discount === "N"
+          ),
+          mf: result[5].rows.filter(
+            (item) => item.enterprise_key === "MF" && item.is_discount === "N"
+          ),
+        };
 
-        if (!result[enterprise_key]) {
-          result[enterprise_key] = [];
-        }
+        const discount = {
+          gc: result[5].rows.filter(
+            (item) => item.enterprise_key === "GC" && item.is_discount === "Y"
+          ),
+          mf: result[5].rows.filter(
+            (item) => item.enterprise_key === "MF" && item.is_discount === "Y"
+          ),
+        };
 
-        result[enterprise_key].push(rest);
-        return result;
-      }, {});
+        const tax = {
+          gc: result[6].rows.filter((item) => item.enterprise_key === "GC"),
+          mf: result[6].rows.filter((item) => item.enterprise_key === "MF"),
+        };
 
-      const shippingCost = {
-        gc: result[5].rows.filter(
-          (item) => item.enterprise_key === "GC" && item.is_discount === "N"
-        ),
-        mf: result[5].rows.filter(
-          (item) => item.enterprise_key === "MF" && item.is_discount === "N"
-        ),
-      };
-
-      const discount = {
-        gc: result[5].rows.filter(
-          (item) => item.enterprise_key === "GC" && item.is_discount === "Y"
-        ),
-        mf: result[5].rows.filter(
-          (item) => item.enterprise_key === "MF" && item.is_discount === "Y"
-        ),
-      };
-
-      const tax = {
-        gc: result[6].rows.filter((item) => item.enterprise_key === "GC"),
-        mf: result[6].rows.filter((item) => item.enterprise_key === "MF"),
-      };
-
-      const data = {
-        totalStats: result[1].rows,
-        chartSeries: Object.values(groupedChartSeries),
-        salesCategories: groupedSalesCategoriesData,
-        topItemsData: Object.values(groupedTopItemsData),
-      };
-
-      res.status(200).json({
-        MFData: {
-          name: "MF",
-          totalStats: {
-            ...data.totalStats[1],
-            line_margin: +Math.round(data.totalStats[1].line_margin)
-              ? +Math.round(data.totalStats[1].line_margin)
-              : 0,
-            line_inventory_cost: +Math.round(
-              data.totalStats[1].line_inventory_cost
-            ),
-            original_order_total_amount: +Math.round(
-              result[7].rows[1].original_order_total_amount
-            ),
-            shipping_cost: +Math.round(shippingCost.mf[0].sum),
-            discount: +Math.round(discount.mf[0].sum),
-            tax: +Math.round(tax.mf[0].sum),
+        const data = {
+          totalStats: result[1].rows,
+          chartSeries: Object.values(groupedChartSeries),
+          salesCategories: groupedSalesCategoriesData,
+          topItemsData: Object.values(groupedTopItemsData),
+        };
+        console.log(totalStats[1]);
+        res.status(200).json({
+          MFData: {
+            name: "MF",
+            totalStats: {
+              ...data.totalStats[1],
+              line_margin: +Math.round(data.totalStats[1]?.line_margin)
+                ? +Math.round(data.totalStats[1]?.line_margin)
+                : 0,
+              line_inventory_cost: +Math.round(
+                data.totalStats[1].line_inventory_cost
+              ),
+              original_order_total_amount: +Math.round(
+                result[7].rows[1].original_order_total_amount
+              ),
+              shipping_cost: +Math.round(shippingCost.mf[0].sum),
+              discount: +Math.round(discount.mf[0].sum),
+              tax: +Math.round(tax.mf[0].sum),
+            },
+            chartSeries: data.chartSeries[1],
+            salesCategories: {
+              ...data.salesCategories[1],
+              ORDER_CAPTURE_CHANNEL_GROUPED: Object.values(
+                data.salesCategories[1].ORDER_CAPTURE_CHANNEL_GROUPED
+              ),
+              ITEM_INFO_GROUPED: Object.values(
+                data.salesCategories[1].ITEM_INFO_GROUPED
+              ),
+            },
+            topItemsData: data.topItemsData[1],
           },
-          chartSeries: data.chartSeries[1],
-          salesCategories: {
-            ...data.salesCategories[1],
-            ORDER_CAPTURE_CHANNEL_GROUPED: Object.values(
-              data.salesCategories[1].ORDER_CAPTURE_CHANNEL_GROUPED
-            ),
-            ITEM_INFO_GROUPED: Object.values(
-              data.salesCategories[1].ITEM_INFO_GROUPED
-            ),
+          GCData: {
+            name: "GC",
+            totalStats: {
+              ...data.totalStats[0],
+              line_margin: +Math.round(data.totalStats[0]?.line_margin)
+                ? +Math.round(data.totalStats[0]?.line_margin)
+                : 0,
+              line_inventory_cost: +Math.round(
+                data.totalStats[0].line_inventory_cost
+              ),
+              original_order_total_amount: +Math.round(
+                result[7].rows[0].original_order_total_amount
+              ),
+              shipping_cost: +Math.round(shippingCost.gc[0].sum),
+              discount: +Math.round(discount.gc[0].sum),
+              tax: +Math.round(tax.gc[0].sum),
+            },
+            chartSeries: data.chartSeries[0],
+            salesCategories: {
+              ...data.salesCategories[0],
+              ORDER_CAPTURE_CHANNEL_GROUPED: Object.values(
+                data.salesCategories[0].ORDER_CAPTURE_CHANNEL_GROUPED
+              ),
+              ITEM_INFO_GROUPED: Object.values(
+                data.salesCategories[0].ITEM_INFO_GROUPED
+              ),
+            },
+            topItemsData: data.topItemsData[0],
           },
-          topItemsData: data.topItemsData[1],
-        },
-        GCData: {
-          name: "GC",
-          totalStats: {
-            ...data.totalStats[0],
-            line_margin: +Math.round(data.totalStats[0].line_margin)
-              ? +Math.round(data.totalStats[0].line_margin)
-              : 0,
-            line_inventory_cost: +Math.round(
-              data.totalStats[0].line_inventory_cost
-            ),
-            original_order_total_amount: +Math.round(
-              result[7].rows[0].original_order_total_amount
-            ),
-            shipping_cost: +Math.round(shippingCost.gc[0].sum),
-            discount: +Math.round(discount.gc[0].sum),
-            tax: +Math.round(tax.gc[0].sum),
-          },
-          chartSeries: data.chartSeries[0],
-          salesCategories: {
-            ...data.salesCategories[0],
-            ORDER_CAPTURE_CHANNEL_GROUPED: Object.values(
-              data.salesCategories[0].ORDER_CAPTURE_CHANNEL_GROUPED
-            ),
-            ITEM_INFO_GROUPED: Object.values(
-              data.salesCategories[0].ITEM_INFO_GROUPED
-            ),
-          },
-          topItemsData: data.topItemsData[0],
-        },
-      });
-    }
-  });
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
