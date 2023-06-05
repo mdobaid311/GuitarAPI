e8dd1bda0db42bbf7fddba817add60446037a3ac
MohdMahebub <mahebub.m@xitsolutionsinc.com>
2023-06-05 18:15:19 +0530

    creatd getTimeSeriesData API

--- v2/routes/tables.js
+++ v2/routes/tables.js
@@ -9,6 +9,7 @@ const {
   UserRegistration,
   getCustomQueryDate,
   getMapData,
+  getTimeSeriesData
 } = require("../controllers/tables");
 
 const router = express.Router();
@@ -21,5 +22,6 @@ router.post("/login", UserLogin);
 router.get("/getMinMaxValues", getMinMaxValues);
 router.post("/register", UserRegistration);
 router.get("/map", getMapData);
+router.get("/timeSeriesData", getTimeSeriesData);
 
 module.exports = router;
